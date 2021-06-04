50c134f9ad9dd2f2d4b09db1c92a55b33c5cb80c
Deelaka Rathnayake <deelaka98@gmail.com>
2021-06-04 18:22:00 +0530

    Create doa.php

---
+++ admin/doa.php
@@ -0,0 +1,50 @@
+<?php 
+	session_start(); 
+
+	if (!isset($_SESSION['username']) && $_SESSION['accesslevel'] !='doa') {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: ../index.php');
+	}
+
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: ../index.php");
+	}
+
+?>
+
+
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="../../css/pure.css">
+				<link rel="stylesheet" href="./panelstyles.css">
+				</head>
+				<?php include('../header.php') ?>
+				<body>
+
+<div class="links_wrapper">
+
+<div class="links_bucket">
+
+
+
+<a class="pure-button pure-button-primary" href="./charts/statshome.php">Stats</a>
+<a class="pure-button pure-button-primary" href="./tb/">All Reports</a>
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+				</body>
+			</html>
