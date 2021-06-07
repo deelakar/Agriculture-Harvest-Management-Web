0bf8ad7eb4c57fd333d06542e72faa61dd23bc1c
Deelaka Rathnayake <deelaka98@gmail.com>
2021-06-07 21:12:28 +0530

    Create db.php

---
+++ main/db.php
@@ -0,0 +1,24 @@
+    <?php
+    /*
+
+	Designed and Developed by Group 36 19.2 PLY SE
+	2021-01-07
+
+	for localhost use : Uni Server Zero XIV
+	
+	Uniform Server Version : 14.0.2
+	Server : PHP 7.4.5 (cli) (built: Apr 14 2020 16:19:14) ( ZTS Visual C++ 2017 x86 )
+	Database : mysql  Ver 8.0.18 for Win64 on x86_64 (MySQL Community Server - GPL)
+	Check mySQL version, may cause fatal query errors due to version compatibility.
+
+	*/
+
+
+    $host = "127.0.0.1";
+    $username = "root";
+    $pass = "abc";
+    $database = "vmarket";
+
+    $db = mysqli_connect($host, $username, $pass, $database);
+
+    ?>
